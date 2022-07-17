e38e397bb333f88590994a1e62ecade500d6b39e
Mitchell Hynes <mpeterhynes@gmail.com>
2022-07-17 17:23:22 -0400

    adds a bootstrap template borrowed from internet

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html") 
+
+@app.route("/blog")
+def linux_blog(post=None):
+    return render_template("linuxblog.html")
+
